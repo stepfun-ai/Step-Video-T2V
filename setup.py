bacc80032374a44b7ec45031d650bdf8ee64b184
fengliaoyuan <fengliaoyuan@bytedance.com>
2025-02-26 00:25:13 +0800

    fix：Fix Hopper architecture FP calc errors via PyTorch/cublas upgrade

--- setup.py
+++ setup.py
@@ -25,8 +25,8 @@ if __name__ == "__main__":
         author="Step-Video Team",
         packages=find_packages(),
         install_requires=[
-            "torchvision==0.18",
-            "torch==2.3",
+            "torchvision==0.20",
+            "torch==2.5.0",
             "accelerate>=1.0.0",
             "transformers>=4.39.1",
             "diffusers>=0.31.0",
